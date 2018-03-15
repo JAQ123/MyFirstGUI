5cedcfdce09f961ace077f509432e8ef5ad4c11a
Julian <julian.quinones@upr.edu>
2018-03-15 17:41:57 -0400

    Initial commit

---
+++ MyFirstGUI/src/main.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class main 
+{
+	public static void main (String[] args)
+	{
+		JFrame mainFrame = new JFrame();
+		
+		mainFrame.setSize(1280,720);
+		
+		mainFrame.setTitle("El GUI");
+		
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		mainFrame.setVisible(true);
+	}
+}
